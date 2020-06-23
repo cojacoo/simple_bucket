ecacf8e880dc76685593950e51e86e0ede8052d8
Conrad Jackisch <jackisch@kit.edu>
2020-06-23 14:08:42 +0200

    advanced

--- bucket_streamlit.py
+++ bucket_streamlit.py
@@ -7,6 +7,8 @@ import numpy as np
 #import altair as alt
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
+from PIL import Image
 
 st.title('Simple Bucket Models')
 
@@ -23,14 +25,28 @@ def oneevent(colp, event_begin = pd.to_datetime('2012-07-13 14:00:00'), event_en
     event_runoff = colp.loc[event_begin:event_end,'runoff'].values
     return colp.loc[event_begin:event_end,['precip','runoff']],event_precip, event_runoff
 
+def longevent(colp, event_begin = pd.to_datetime('2012-05-01 14:00:00'), event_end = pd.to_datetime('2012-10-20 14:00:00')):
+    #Select one runoff event manulally
+    
+    event_precip = colp.loc[event_begin:event_end,'precip'].values
+    event_runoff = colp.loc[event_begin:event_end,'runoff'].values
+    return colp.loc[event_begin:event_end,['precip','runoff']],event_precip, event_runoff
+
+
 colp = load_data()
-colp1, P, Q = oneevent(colp)
+
+eventi = st.checkbox('Lange Zeitreihe')
+if eventi:
+    colp1, P, Q = longevent(colp)
+else:
+    colp1, P, Q = oneevent(colp)
 
 #Plot selected event
 st.subheader('Some Rainfall Runoff Data')
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=colp1.index,y=colp1.precip, mode='lines+markers', hovertext=colp1.precip, name='Precipitation'))
-fig.add_trace(go.Scatter(x=colp1.index,y=colp1.runoff, mode='lines+markers', hovertext=colp1.runoff, name='Runoff'))
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(go.Scatter(x=colp1.index,y=colp1.precip, mode='lines', hovertext=colp1.precip, name='Precipitation'),secondary_y=False)
+fig.add_trace(go.Scatter(x=colp1.index,y=colp1.runoff, mode='lines', hovertext=colp1.runoff, name='Runoff'),secondary_y=True)
 xatitle='time'
 yatitle='mm/h'
 
@@ -81,9 +97,16 @@ st.plotly_chart(fig)
 st.sidebar.title('Settings')
 MO = st.sidebar.selectbox('Select Model',['Linear','Beta Store'])
 if MO=='Linear':
+
+    image = Image.open('linstore.png')
+    st.sidebar.image(image,use_column_width=True)
+    
+    st.sidebar.markdown('Mittlere Verweilzeit des Wassers im Speicher:')
     tres = st.sidebar.slider('Residence time (days)', 30, 300, 100)
+    st.sidebar.markdown('Verdunstung (als Senkenterm):')
     ETx = st.sidebar.slider('ET (mm/day)', 1., 10., 3.)
     ET = ETx/24.
+    st.sidebar.markdown('Initiale Speicherfüllung:')
     sini = st.sidebar.slider('Initial Storage', 1., 10., 2.5)
 
     #initialisation
@@ -99,8 +122,8 @@ if MO=='Linear':
     
     st.subheader(MO+' Model')
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=colp1.index,y=runoff, mode='lines+markers', hovertext=colp1.precip, name='Model'))
-    fig.add_trace(go.Scatter(x=colp1.index,y=Q, mode='lines+markers', hovertext=colp1.runoff, name='Runoff'))
+    fig.add_trace(go.Scatter(x=colp1.index,y=runoff, mode='lines', hovertext=colp1.precip, name='Model'))
+    fig.add_trace(go.Scatter(x=colp1.index,y=Q, mode='lines', hovertext=colp1.runoff, name='Runoff'))
     
     fig.update_layout(
         xaxis=dict(
@@ -147,12 +170,22 @@ if MO=='Linear':
     st.plotly_chart(fig)
 
 elif MO=='Beta Store':
+
+    image = Image.open('beta.png')
+    st.sidebar.image(image,use_column_width=True)
+
+    st.sidebar.markdown('Mittlere Verweilzeit des Wassers im Gewässerspeicher:')
     tres = st.sidebar.slider('Residence time (days)', 30, 300, 100)
+    st.sidebar.markdown('$\beta$-Parameter im Bodenspeicher:')
     beta = st.sidebar.slider('beta exponent', 0.3, 3., 1.25)
+    st.sidebar.markdown('Größe des Bodenspeichers als Porosität einer 1m Bodensäule:')
     poro = st.sidebar.slider('porosity', 0.2, 0.5, 0.3)
+    st.sidebar.markdown('Verdunstung (als Senkenterm):')
     ETx = st.sidebar.slider('ET (mm/day)', 1., 10., 3.)
     ET = ETx/24.
+    st.sidebar.markdown('Initiale Speicherfüllung (Gewässer):')
     sini = st.sidebar.slider('Initial Storage', 1., 10., 2.5)
+    st.sidebar.markdown('Initiale Speicherfüllung (Boden):')
     tini = st.sidebar.slider('Initial Soil', 100., 300., 150.)
 
     smax = poro * 1000  #Maximum storage as product of soil porosity and soil depth in mm
@@ -178,8 +211,8 @@ elif MO=='Beta Store':
 
     st.subheader(MO+' Model')
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=colp1.index,y=runoff, mode='lines+markers', hovertext=colp1.precip, name='Model'))
-    fig.add_trace(go.Scatter(x=colp1.index,y=Q, mode='lines+markers', hovertext=colp1.runoff, name='Runoff'))
+    fig.add_trace(go.Scatter(x=colp1.index,y=runoff, mode='lines', hovertext=colp1.precip, name='Model'))
+    fig.add_trace(go.Scatter(x=colp1.index,y=Q, mode='lines', hovertext=colp1.runoff, name='Runoff'))
     
     fig.update_layout(
         xaxis=dict(
