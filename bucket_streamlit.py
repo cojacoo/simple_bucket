984b4592d3087f4f6720ce32ae4953a5b001a027
Conrad Jackisch <jackisch@kit.edu>
2020-06-23 08:59:33 +0200

    corrected

--- bucket_streamlit.py
+++ bucket_streamlit.py
@@ -3,9 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pydeck
-import glob
-import statsmodels.api as sm
+#import statsmodels.api as sm
 #import altair as alt
 import plotly.graph_objects as go
 import plotly.express as px
